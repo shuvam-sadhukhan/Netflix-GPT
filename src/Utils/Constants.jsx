4b3f9922143a8bd8b382c9920fc4e3b3eaf8a0ab
shuvam-sadhukhan <sadhukhanshuvam63@gmail.com>
2025-09-18 11:37:49 +0530

    adding files

--- src/Utils/Constants.jsx
+++ src/Utils/Constants.jsx
@@ -2,7 +2,7 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYmZjNDc4OGM3OThkNjg1MDcwZGE1YzAxZjAzODlhOCIsIm5iZiI6MTc1NTg0MTk0MS42NDQsInN1YiI6IjY4YTgwNTk1NzI4MTRmMGQzNmM1NmU5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BkRJGpvUSC1RfpG44QUrjvvdQOqxcyVGRXq-06efwnA'
+    Authorization: 'Bearer '+ import.meta.env.VITE_TMDB_KEY
   }
 };
 
